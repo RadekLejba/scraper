d0591edff00d853bc7105b55661977f805878dbb
Radek lejba <radeklejba@gmail.com>
2019-01-18 15:16:19 +0100

    INITIAL

---
+++ scraper/spiders/text_spider.py
@@ -0,0 +1,18 @@
+from itertools import count
+
+import scrapy
+
+
+class Textspider(scrapy.Spider):
+    name = "text"
+
+    def start_requests(self):
+        url = 'http://fuzzle.pl/'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
